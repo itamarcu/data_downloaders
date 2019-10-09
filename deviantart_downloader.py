81a6bf4c7cbc977348a9016aed92523a782701c7
Itamar Curiel <itamarcu@gmail.com>
2019-10-09 16:06:14 +0300

    Update deviantart_downloader to work with Eclipse, though badly, because it cannot use the download link (so most images won't be at highest resolution)

--- deviantart_downloader.py
+++ deviantart_downloader.py
@@ -1,15 +1,14 @@
 import os
-import re
 import shutil
 import requests
 from bs4 import BeautifulSoup
 
 
 class Deviation:
-    def __init__(self, combined_name: str, url: str, image_url: str):
-        self.combined_name = combined_name
+    def __init__(self, deviation_name: str, artist_username: str, url: str):
+        self.deviation_name = deviation_name
+        self.artist_username = artist_username
         self.url = url
-        self.image_url = image_url
 
 
 def download_collection(collection_url, output_folder=None, name_format=None):
@@ -27,37 +26,41 @@ def download_collection(collection_url, output_folder=None, name_format=None):
     Enjoy!"""
     print(f" ♢ Connecting to DeviantArt ({collection_url})…")
 
-    collection_page = requests.get(collection_url)
-    collection_page_soup = BeautifulSoup(collection_page.content, "html.parser")
+    collection_page_response = requests.get(collection_url)
+    collection_page_soup = BeautifulSoup(collection_page_response.content, "html.parser")
 
-    collection_name = collection_page_soup.select(".folder-title")[0].text
+    collection_name = collection_page_soup.select(".uUWfu")[0].text
     print(f" ♢ Found collection: {collection_name}")
     print(f" ♢ Collecting info about all deviations…")
     deviations = []
-    for thing1 in collection_page_soup.select("#gmi-"):
-        for thing2 in thing1.findChildren("span", recursive=False):
-            deviations.append(Deviation(
-                combined_name=thing2["data-super-alt"],  # "X by Y"
-                url=thing2["href"],
-                image_url=thing2["data-super-full-img"],
-            ))
+    for thing in collection_page_soup.select("._2Pnr5"):
+        style_text = thing.select("._3ApeV.RZk0d")[0]["style"]
+        deviations.append(Deviation(
+            deviation_name=thing.select("._1TFfi")[0].text,
+            artist_username=thing.select(".user-link._2diFW")[0]["title"],
+            url=thing.select("._2vta_")[0]["href"],
+        ))
 
     if output_folder is None:
         current_directory = os.getcwd()
         images_directory = os.path.join(current_directory, collection_name)
     else:
         images_directory = output_folder
+    if name_format is None:
+        name_format = "{deviation_name} by {artist_username}"
     if not os.path.exists(images_directory):
         os.makedirs(images_directory)
     print(f" ♢ Downloading everything into {images_directory}")
     total_size = 0
     for dev in deviations:
-        response = requests.get(dev.image_url, stream=True)
+        deviation_response = requests.get(dev.url)
+        deviation_soup = BeautifulSoup(deviation_response.content, "html.parser")
+        image_picture_url = deviation_soup.select("._1LGGs > div > img")[0]["src"]
+        response = requests.get(image_picture_url, stream=True)
         content_length = int(response.headers['Content-length'])
         total_size += content_length
         mb_size = f"{content_length / 1_048_576:.2f} MB"
-        deviation_name, artist_username = re.match("(.*) by (.*?)$", dev.combined_name).groups()
-        name = name_format.format(deviation_name=deviation_name, artist_username=artist_username)
+        name = name_format.format(deviation_name=dev.deviation_name, artist_username=dev.artist_username)
         name = name.replace("..", " ").replace("/", "_")
         print(mb_size+"\t "+name)
         with open(os.path.join(images_directory, f"{name}.png"), "wb") as out_file:
