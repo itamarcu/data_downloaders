b1d46004c0d23da1ed158d9c7bbb769003f3e5a3
Itamar Curiel <itamarcu@gmail.com>
2019-12-25 00:47:33 +0200

    Update the dnd random spell downloader thing to use a local copy of the spell list and update it only when necessary; also, better filtering for material components

--- dnd_5e_spells_downloader.py
+++ dnd_5e_spells_downloader.py
@@ -1,3 +1,4 @@
+import json
 import random
 import re
 import sys
@@ -11,16 +12,25 @@ Spell = Dict[str, Any]
 
 
 def main():
-    if len(sys.argv) != 2:
-        raise TypeError("Missing argument: level")
+    usage_string = f"Usage: {sys.argv[0]} level [--no-material-cost | --no-consumable-material-cost]"
+    if not 2 <= len(sys.argv) <= 3:
+        raise TypeError(usage_string)
     level = int(sys.argv[1])
+    allowed_material_costs = "all"
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "--no-material-cost":
+            allowed_material_costs = "none"
+        elif sys.argv[2] == "--no-consumable-material-cost":
+            allowed_material_costs = "only non-consumable costs"  # e.g. scrying is allowed but illusory script isn't
+        else:
+            raise TypeError(usage_string)
     all_spells = download_all_spells()
     random.seed(Date.today().isoformat())
     print(f"---Drawing random spells for level {level}---")
     sampled_spells = sample_random_spells(
         all_spells,
         spell_slots_by_level(level),
-        allow_material_costs=True,
+        allowed_material_costs=allowed_material_costs,
     )
     for spell in sampled_spells:
         print(spell_to_str(spell))
@@ -31,38 +41,66 @@ ALLOWED_SOURCES = [
     "Guildmasters' Guide to Ravnica",
     "Sword Coast Adventurer's Guide",
     "Xanathar's Guide to Everything",
+    "UAFighterRogueWizard, UAModernMagic, UASorcererAndWarlock, UAStarterSpells, UAThatOldBlackMagic"
 ]
 
 
 def download_all_spells():
-    print("Checking index...")
-    version_translations_response = requests.get("https://5etools.com/js/header.js")
-    inv_version_translations_1 = re.findall(r'"book\.html","([^"]+)",{aHash:"([^"]+)"}',
-                                            version_translations_response.text)
-    inv_version_translations_2 = re.findall(r'"adventure\.html","([^"]+)",{isSide:!0,aHash:"([^"]+)"}',
-                                            version_translations_response.text)
-    inv_version_translations = inv_version_translations_1 + inv_version_translations_2
-    version_translations = {shortname: fullname for fullname, shortname in inv_version_translations}
-    index_response = requests.get("https://5etools.com/data/spells/index.json")
-    all_sources: dict = index_response.json()
-    all_sources = {version_translations.get(shortname, shortname): all_sources[shortname] for shortname in all_sources}
-    sources = {}
+    print("Checking version...")
+    get_5e_tools = requests.get("https://get.5e.tools/")
+    latest_version = re.search(r"release/5eTools.(.*?).zip", get_5e_tools.text).group(1)
+    with open("dnd_5etools_metadata.json") as file:
+        metadata = json.load(file)
+        downloaded_version = metadata["version"]
+    file_path = "dnd_5e_all_spells.json"
+    if downloaded_version == latest_version:
+        print(f"Already got latest 5etools version ({latest_version}).")
+        print(f"Reading data from {file_path}")
+        with open(file_path, encoding="utf8") as file:
+            data = json.load(file)
+            all_sources = data["all_sources"]
+            all_spells = data["all_spells"]
+            source_map = data["source_map"]
+    else:
+        print(f"Local version is {downloaded_version} but server has version {latest_version}. Updating local data...")
+        source_map_response = requests.get("https://5etools.com/js/header.js")
+        inv_source_map_1 = re.findall(r'"book\.html","([^"]+)",{aHash:"([^"]+)"}',
+                                      source_map_response.text)
+        inv_source_map_2 = re.findall(r'"adventure\.html","([^"]+)",{isSide:!0,aHash:"([^"]+)"}',
+                                      source_map_response.text)
+        inv_source_map = inv_source_map_1 + inv_source_map_2
+        source_map = {shortname: fullname for fullname, shortname in inv_source_map}
+        index_response = requests.get("https://5etools.com/data/spells/index.json")
+        all_sources: dict = index_response.json()  # will map long names to short names
+        all_sources = {full_source(source_map, shortname): all_sources[shortname] for shortname in all_sources}
+        all_spells = []
+        for source_name in all_sources:
+            print(f"Downloading spells from {source_name}...", end="")
+            spells = download_spells_from_source(all_sources[source_name])
+            all_spells.extend(spells)
+            print(f"...Done. ({len(spells)} spells)")
+        print(f"Saving data to {file_path}...")
+        data = {
+            "all_sources": all_sources,
+            "all_spells": all_spells,
+            "source_map": source_map,
+        }
+        with open(file_path, "w", encoding="utf8") as file:
+            with open("dnd_5etools_metadata.json", "w") as meta_file:
+                file.write(json.dumps(data, indent=2, ensure_ascii=False))
+                metadata["version"] = latest_version
+                meta_file.write(json.dumps(metadata, indent=2, ensure_ascii=False))
+    allowed_sources = []
     removed_sources = []
     for source in all_sources:
-        if source in ALLOWED_SOURCES:
-            sources[source] = all_sources[source]
-        else:
-            removed_sources.append(source)
+        (allowed_sources if source in ALLOWED_SOURCES else removed_sources).append(source)
+    print(f"Allowed sources: {', '.join(s for s in allowed_sources)}")
     if any(removed_sources):
         print(f"Removed sources: {', '.join(s for s in removed_sources)}")
-    all_spells = []
-    for source in sources:
-        print(f"Downloading spells from {source}...", end="")
-        spells = download_spells_from_source(sources[source])
-        all_spells.extend(spells)
-        print(f"...Done. ({len(spells)} spells)")
-    print(f"All done. ({len(all_spells)} spells)")
-    return all_spells
+    allowed_sources = set(allowed_sources)
+    all_allowed_spells = [spell for spell in all_spells if full_source(source_map, spell["source"]) in allowed_sources]
+    print(f"All done. ({len(all_allowed_spells)}/{len(all_spells)} spells)")
+    return all_allowed_spells
 
 
 def download_spells_from_source(source_suffix: str):
@@ -70,23 +108,48 @@ def download_spells_from_source(source_suffix: str):
     return spells_response.json()["spell"]
 
 
+def full_source(source_map: dict, short_source_name: str) -> str:
+    return source_map.get(short_source_name, short_source_name)
+
+
 def sample_random_spells(
         all_spells: List[Spell],
         ssbsl: Dict[int, int],
-        allow_material_costs: bool = True,
+        allowed_material_costs: str = "all",
 ) -> List[Spell]:
     """for each spell slot, samples a random spell that fits that slot.
 
     :param all_spells: list of all spells in the game
     :param ssbsl: dict of spell slots by spell level, like the one returned by spell_slots_by_level()
-    :param allow_material_costs: if False, will filter out all spells with material cost, consumed or not"""
+    :param allowed_material_costs: filter out all with material cost; can be "all", "none", or "only non-consumable costs"."""
     sampled_spells = []
     for spell_level, count in ssbsl.items():
         eligible_spells = [spell for spell in all_spells if spell["level"] == spell_level]
-        if not allow_material_costs:
-            eligible_spells = [spell for spell in eligible_spells if type(spell["components"].get("m", "")) == str]
+        if allowed_material_costs == "all":
+            pass
+        elif allowed_material_costs == "none":
+            eligible_spells = [spell for spell in eligible_spells if material_component_type(spell) in ["no", "free"]]
+        elif allowed_material_costs == "only non-consumable costs":
+            eligible_spells = [spell for spell in eligible_spells if material_component_type(spell) != "consumed"]
+        else:
+            raise ValueError("")
         sampled_spells.extend(random.sample(eligible_spells, count))
     return sampled_spells
+
+
+def material_component_type(spell: Spell) -> str:
+    if "m" not in spell["components"]:
+        return "no"  # no material component; e.g. Catapult
+    m = spell["components"]["m"]
+    if type(m) == str:
+        mm = m
+    else:
+        mm = m["text"]
+    if "worth" not in mm:
+        return "free"  # flavor-text material component; e.g. Fireball, "a tiny ball of bat guano and sulfur"
+    if "consume" not in mm:
+        return "pricey"  # pricey yet one-time-cost component; e.g. Chromatic Orb, "a diamond worth at least 50 gp"
+    return "consumed"  # consumed per casting; e.g. Ceremony, "25 gp worth of powdered silver, which the spell consumes"
 
 
 def spell_to_str(spell: Spell) -> str:
